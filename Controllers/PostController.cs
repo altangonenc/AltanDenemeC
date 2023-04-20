5c39f704e9e169846af2f4c95add36ee9ace564e
fiseqqq <altangonenc10@gmail.com>
2023-04-20 23:32:20 +0300

    first commit

---
+++ Controllers/PostController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltanDenemeC.Controllers
+{
+    [ApiController]
+    public class PostController:ControllerBase
+    {
+        []
+        public ActionResult GetAll(){
+            return Ok();
+        }
+    }
+}
