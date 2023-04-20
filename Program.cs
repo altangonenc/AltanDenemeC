660335b084f620fa81e6744da9599062a8b5a351
fiseqqq <altangonenc10@gmail.com>
2023-04-21 02:35:42 +0300

    databaseContext oluşsturuldu******onemli*

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using AltanDenemeC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("cnnstr"); //configuration entity f. core tools ile birlikte geliyor.
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
